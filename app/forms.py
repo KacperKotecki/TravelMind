4f7391587471247aa06394e1bdd190b5101a2b21
Weronika Kusidel <kusidelw@gmail.com>
2025-11-19 15:47:31 +0100

    dodanie formularza w forms.py oraz dodanie linku do formualrza rejestracji

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,12 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Hasło', validators=[DataRequired()])
     submit = SubmitField('Zaloguj się')
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('Imię', validators=[DataRequired()])
+    last_name = StringField('Nazwisko', validators=[DataRequired()])
+    phone = StringField('Numer telefonu', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    password2 = PasswordField('Powtórz hasło', validators=[DataRequired()])
+    submit = SubmitField('Zarejestruj się')
