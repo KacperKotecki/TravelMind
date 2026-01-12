f9af7a494158f9bc0d7ad1834c9011324dce3156
Kacper Kotecki <kacperkotecki@o2.pl>
2026-01-12 16:25:47 +0100

    Usunołem zbędne importy z app/main/routs.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,9 +10,6 @@ from app.api import build_geocode_variants, search_city_suggestions
 
 from app.models import GeneratedPlan # Import User jest tu zbędny, jeśli nie używasz go bezpośrednio w query
 
-from app import db, mail
-from flask_mail import Message
-from app.forms import  RegistrationForm, RequestResetForm, ResetPasswordForm
 
 # Krok 5: Przygotowanie stałych finansowych
 # Bazowe koszty dzienne w PLN dla różnych stylów podróży (mnożnik 1.0)
